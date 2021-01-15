83a1a6c692ae54cd71f5e4c947e80fba838c4420
jbpark <jungbyung8@gmil.com>
2021-01-15 14:39:26 +0900

    renewal

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const PORT = 8888;
+const express = require('express')
+const app = express()
+const PORT = 8888
 
 app.get('/', (req,res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`);
-});
+})
